a38a8abdbbf28751371c69e7864efe3cd11c9d47
andrei <35298550+d4rckh@users.noreply.github.com>
2024-07-25 15:15:12 +0300

    add controller, service

---
+++ andrei-s/src/main/java/org/java/mentorship/andrei_s/config/TestConfiguration.java
@@ -0,0 +1,37 @@
+package org.java.mentorship.andrei_s.config;
+
+import org.java.mentorship.andrei_s.service.TestService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    @Primary
+    Map<String, String> mapOfConfiguration(TestService testService)
+    {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("prop1", "val1");
+        map.put("prop2", "val2");
+
+        return map;
+    }
+
+    @Bean
+    Map<String, String> secondMapOfConfiguration()
+    {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("secondProp1", "val1");
+        map.put("secondProp2", "val2");
+
+        return map;
+    }
+
+}
