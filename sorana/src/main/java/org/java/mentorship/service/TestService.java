4de91a4190706b457fb395a38c284f426964711c
Sorana09 <sorananeculaed@gmail.com>
2024-07-26 00:14:47 +0300

    some get and push methods :))

--- sorana/src/main/java/org/java/mentorship/service/TestService.java
+++ sorana/src/main/java/org/java/mentorship/service/TestService.java
@@ -3,11 +3,64 @@ package org.java.mentorship.service;
 import org.java.mentorship.domain.Song;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 @Service
 public class TestService {
-    public Song getSong(){
-        Song song = new Song();
 
-        return song;
+    List<Song> songs;
+
+    TestService(){
+        this.songs = new ArrayList<>();
+
+        this.songs.add(new Song(1, "Pop", 10, 10));
+        this.songs.add(new Song(2, "Pop", 10, 10));
+        this.songs.add(new Song(3, "Rock", 10, 11));
+        this.songs.add(new Song(4, "Rock", 10, 11));
+        this.songs.add(new Song(4, "Rock", 10, 12));
+    }
+
+
+    public List<Song> getSongs(){
+        return this.songs;
+    }
+
+    public List<Song> getSongsByStyle(String style){
+
+        if(style == null) return this.songs;
+
+        List<Song> songsByStyle = new ArrayList<>();
+
+        for(Song song: this.songs){
+            if(song.getStyle().equals(style)){
+                songsByStyle.add(song);
+            }
+        }
+        return songsByStyle;
+    }
+
+    public Song getSongById(int id){
+        if(id == 0) return null;
+        for(Song song: this.songs){
+            if(song.getId() == id){
+                return song;
+            }
+        }
+        return null;
+    }
+
+    public boolean addSong1(Song song){
+
+        if(this.songs.isEmpty())
+            song.setId(1);
+        else song.setArtistId(this.songs.getLast().getId()+1);
+
+        if(song.getArtistId() != 0 && song.getStyle() !=null && song.getAlbumId()!=0){
+            this.songs.add(song);
+            return true;
+        }
+        else return false;
     }
 }
