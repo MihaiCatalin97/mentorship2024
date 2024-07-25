499a43a1da287287f5a0739fe199b3210551b617
d4rckh <35298550+d4rckh@users.noreply.github.com>
2024-07-25 20:49:04 +0300

    add sample data to linkedlist and create GET /songs/{id} endpoint

--- andrei-s/src/main/java/org/java/mentorship/andrei_s/domain/Song.java
+++ andrei-s/src/main/java/org/java/mentorship/andrei_s/domain/Song.java
@@ -1,8 +1,11 @@
 package org.java.mentorship.andrei_s.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class Song {
     private int id;
 
