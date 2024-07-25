c95de046ea2ddcef3c2f0a7089e9e352869ddf79
andrei <35298550+d4rckh@users.noreply.github.com>
2024-07-25 12:32:14 +0300

    Create module andrei-s using spring initalizr

---
+++ andrei-s/src/main/java/org/java/mentorship/andrei_s/AndreiSApplication.java
@@ -0,0 +1,13 @@
+package org.java.mentorship.andrei_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndreiSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndreiSApplication.class, args);
+	}
+
+}
---
+++ andrei-s/src/test/java/org/java/mentorship/andrei_s/AndreiSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.mentorship.andrei_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndreiSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
