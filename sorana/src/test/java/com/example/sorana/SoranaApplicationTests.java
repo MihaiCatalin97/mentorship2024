34b0631b5270548a5e5b63956dcffdafff97dff5
Sorana09 <sorananeculaed@gmail.com>
2024-07-25 12:34:42 +0300

    ;))

---
+++ sorana/src/test/java/com/example/sorana/SoranaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sorana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
