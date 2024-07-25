fc051a8caef501a7ac382c6e87e82f29e208f566
MihaiCatalin97 <mihai.catalin197@gmail.com>
2024-07-25 17:23:36 +0300

    Add endpoint

--- catalin-mihai/src/main/java/org/java/mentorship/service/TestService.java
+++ catalin-mihai/src/main/java/org/java/mentorship/service/TestService.java
@@ -1,7 +1,14 @@
 package org.java.mentorship.service;
 
+import org.java.mentorship.domain.Song;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
+
+    public Song getSong() {
+        Song song = new Song();
+
+        return song;
+    }
 }
