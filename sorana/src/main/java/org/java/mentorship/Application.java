1a02e13781d28d0bbd2bb2701c4432af51628d69
Sorana09 <sorananeculaed@gmail.com>
2024-07-25 17:20:30 +0300

    ;))

--- sorana/src/main/java/org/java/mentorship/Application.java
+++ sorana/src/main/java/org/java/mentorship/Application.java
@@ -20,5 +20,4 @@ public class Application {
             System.out.println(beanName);
         }
     }
-
 }
---
+++ sorana/src/main/java/org/java/mentorship/domain/Album.java
@@ -0,0 +1,8 @@
+package org.java.mentorship.domain;
+
+import lombok.Data;
+
+@Data
+public class Album {
+    private String name;
+}
