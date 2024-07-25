5b395f00bc1fcf5aaca0207e6b28db275372897a
Sorana09 <sorananeculaed@gmail.com>
2024-07-25 15:32:41 +0300

    ;))

---
+++ src/main/java/org/java/mentorship/PojoClass.java
@@ -0,0 +1,13 @@
+package org.java.mentorship;
+
+import lombok.Data;
+
+@Data
+public class PojoClass {
+
+    private String stringField;
+
+    private Integer integerField;
+
+    private int intField;
+}
