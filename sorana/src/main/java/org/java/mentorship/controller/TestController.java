69712e902ece09f8f3efd8898bf6fbd80076aa3e
Sorana09 <sorananeculaed@gmail.com>
2024-07-25 17:20:30 +0300

    ;))

--- sorana/src/main/java/org/java/mentorship/controller/TestController.java
+++ sorana/src/main/java/org/java/mentorship/controller/TestController.java
@@ -1,9 +1,14 @@
 package org.java.mentorship.controller;
 
+import org.java.mentorship.domain.Song;
 import org.java.mentorship.service.TestService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,4 +20,16 @@ public class TestController {
                           @Qualifier("mapOfProperties") Map<String, String> properties) {
         this.testService = testService;
     }
+
+    @GetMapping("/songs")
+    public ResponseEntity<List<Song>> getAllSongs(){
+        Song song = new Song();
+        song.setId(1);
+
+        List<Song> songs = new ArrayList<>();
+        songs.add(song);
+
+        return ResponseEntity.ok(songs);
+    }
+
 }
---
+++ sorana/src/main/java/org/java/mentorship/domain/Song.java
@@ -0,0 +1,12 @@
+package org.java.mentorship.domain;
+
+import lombok.Data;
+
+@Data
+public class Song {
+
+    private int id;
+    private String style;
+    private int artistId;
+    private Album album;
+}
