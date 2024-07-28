d9e67119e533fdbf52f392cb8eda03fd5bfefbdf
SpatariuIoanGabriel <sp.ionut02@gmail.com>
2024-07-28 19:37:19 +0300

    Done

--- ionut/src/main/java/org/java/mentorship/controller/TestController.java
+++ ionut/src/main/java/org/java/mentorship/controller/TestController.java
@@ -1,10 +1,15 @@
 package org.java.mentorship.controller;
 
+import org.java.mentorship.domain.Album;
+import org.java.mentorship.domain.Song;
 import org.java.mentorship.service.TestService;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class TestController {
@@ -14,5 +19,44 @@ public class TestController {
     public TestController(TestService testService,
                           @Qualifier("mapOfProperties") Map<String, String> properties) {
         this.testService = testService;
+    }
+
+    @GetMapping("/songs")
+    public ResponseEntity<List<Song>> getAllSongs() {
+        List<Song> songs = testService.getAllSongs();
+        return ResponseEntity.ok(songs);
+    }
+
+    @GetMapping("/songs/{id}")
+    public ResponseEntity<Song> getSongById(@PathVariable int id) {
+        Optional<Song> song = testService.getSongById(id);
+        return song.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/songs")
+    public ResponseEntity<Song> createSong(@RequestBody Song song) {
+        String style = song.getStyle();
+        int artistId = song.getArtistId();
+        Album album = song.getAlbum();
+
+        Song createdSong = testService.createSong(style, artistId, album);
+
+        return ResponseEntity.ok(createdSong);
+    }
+
+
+    @PutMapping("/songs/{id}")
+    public ResponseEntity<Song> updateSong(@PathVariable int id, @RequestBody Song songDetails) {
+        Optional<Song> updatedSong = testService.updateSong(id, songDetails);
+        return updatedSong.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/songs/{id}")
+    public ResponseEntity<Void> deleteSong(@PathVariable int id) {
+        boolean isDeleted = testService.deleteSong(id);
+        return isDeleted ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
     }
 }
---
+++ ionut/src/main/java/org/java/mentorship/domain/Album.java
@@ -0,0 +1,11 @@
+package org.java.mentorship.domain;
+
+import lombok.Data;
+
+@Data
+public class Album {
+
+    private int id;
+
+    private int artistId;
+}
