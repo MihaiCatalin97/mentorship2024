a7b3d2b94ed13d5d987f1da66930a028002f4c77
SpatariuIoanGabriel <sp.ionut02@gmail.com>
2024-07-28 19:37:19 +0300

    Done

--- ionut/src/main/java/org/java/mentorship/service/TestService.java
+++ ionut/src/main/java/org/java/mentorship/service/TestService.java
@@ -1,7 +1,46 @@
 package org.java.mentorship.service;
 
+import org.java.mentorship.domain.Album;
+import org.java.mentorship.domain.Song;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TestService {
+
+    private final List<Song> songs = new ArrayList<>();
+    private int currentId = 1;
+
+    public List<Song> getAllSongs() {
+        return new ArrayList<>(songs);
+    }
+
+    public Optional<Song> getSongById(int id) {
+        return songs.stream().filter(song -> song.getId() == id).findFirst();
+    }
+
+    public Song createSong(String style, int artistId, Album album) {
+        Song song = new Song(currentId++, style, artistId, album);
+        songs.add(song);
+        return song;
+    }
+
+    public Optional<Song> updateSong(int id, Song songDetails) {
+        Optional<Song> optionalSong = getSongById(id);
+
+        optionalSong.ifPresent(song -> {
+            song.setStyle(songDetails.getStyle());
+            song.setArtistId(songDetails.getArtistId());
+            song.setAlbum(songDetails.getAlbum());
+        });
+
+        return optionalSong;
+    }
+
+    public boolean deleteSong(int id) {
+        return songs.removeIf(song -> song.getId() == id);
+    }
 }
