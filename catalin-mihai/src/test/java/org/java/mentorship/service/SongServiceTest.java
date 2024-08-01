f7e3e76f86030c370e59a0a8e76259037544b7fe
MihaiCatalin97 <mihai.catalin197@gmail.com>
2024-08-01 15:47:35 +0300

    Add unit tests

---
+++ catalin-mihai/src/test/java/org/java/mentorship/service/SongServiceTest.java
@@ -0,0 +1,65 @@
+package org.java.mentorship.service;
+
+import org.java.mentorship.domain.Song;
+import org.java.mentorship.persistence.SongRepository;
+import org.java.mentorship.validation.SongValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SongServiceTest {
+
+    private SongRepository repository;
+
+    @Mock
+    private SongValidator validator;
+
+    private SongService songService;
+
+    @Captor
+    private ArgumentCaptor<Song> songArgumentCaptor;
+
+    @BeforeEach
+    void init() {
+        repository = mock(SongRepository.class);
+
+        songService = new SongService(repository, validator);
+    }
+
+    @Test
+    void saveShouldValidateAndCallRepository() {
+        // Given
+        Song song = new Song();
+        song.setName("Name");
+        song.setStyle("Style");
+
+        when(repository.save(song)).thenAnswer(invocationOnMock -> {
+            Song argument = invocationOnMock.getArgument(0, Song.class);
+
+            Song expected = new Song();
+            expected.setName(argument.getName());
+            expected.setStyle(argument.getStyle());
+            expected.setId(1);
+            return expected;
+        });
+
+        // When
+        Song result = songService.save(song);
+
+        // Then
+        verify(validator, times(1)).validate(songArgumentCaptor.capture());
+        assertEquals(1, result.getId());
+
+        Song capturedSong = songArgumentCaptor.getValue();
+        assertNotNull(capturedSong);
+    }
+}
---
+++ catalin-mihai/src/test/java/org/java/mentorship/validation/SongValidatorTest.java
@@ -0,0 +1,64 @@
+package org.java.mentorship.validation;
+
+import org.java.mentorship.domain.Song;
+import org.java.mentorship.exception.FieldIsNullException;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SongValidatorTest {
+
+    private static String SOMETHING_STATIC;
+
+    private SongValidator songValidator;
+
+    @BeforeAll
+    static void init() {
+        SOMETHING_STATIC = "Ceva static";
+    }
+
+    @BeforeEach
+    void initialize() {
+        songValidator = new SongValidator();
+    }
+
+    /**
+     * methodName Should (Return ceva / Throw ceva / Do nothing) When Parameter is something
+     */
+    @Test
+    void validateShouldThrowExceptionWhenNameIsNull() {
+        // Given
+        Song song = new Song();
+        song.setName(null);
+
+        // When & Then
+        FieldIsNullException exception = assertThrows(FieldIsNullException.class,
+                () -> songValidator.validate(song),
+                "Expected exception to be thrown");
+
+        assertEquals("Field 'name' is null",
+                exception.getMessage(),
+                "Unexpected exception message");
+    }
+
+    @Test
+    void validateShouldDoNothingWhenNameIsNotNull() {
+        // Given
+        Song song = new Song();
+        song.setName("Not null");
+
+        // When & Then
+        assertDoesNotThrow(() -> songValidator.validate(song),
+                "Expected exception to be thrown");
+    }
+
+    @AfterEach
+    void destroy() {
+        songValidator = null;
+    }
+
+    @AfterAll
+    static void destroyFinal() {
+        SOMETHING_STATIC = null;
+    }
+}
