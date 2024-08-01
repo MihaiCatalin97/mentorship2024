c2610a76a66adcfbb37239c73e65c8bbe6f672ce
Ioan Gabriel Spatariu <126616594+SpatariuIoanGabriel@users.noreply.github.com>
2024-08-01 11:35:03 +0300

    Done!

--- ionut/src/main/java/org/java/mentorship/domain/Song.java
+++ ionut/src/main/java/org/java/mentorship/domain/Song.java
@@ -3,14 +3,14 @@ package org.java.mentorship.domain;
 import lombok.Builder;
 import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
-@Builder
+@Builder @Jacksonized
 @AllArgsConstructor
 public class Song {
-
     private int id;
     private String style;
     private int artistId;
-    private Album album;
+    private int albumId;
 }
