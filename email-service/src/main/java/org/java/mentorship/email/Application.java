fb167eecd8851dffa59078df0547b38f7afaa683
DonAndrei <64136588+DonAndrei@users.noreply.github.com>
2024-08-08 11:10:09 +0300

    Creation of email service (#21)
    * Added email service branch
    
    * Setup of email service

---
+++ email-service/src/main/java/org/java/mentorship/email/Application.java
@@ -0,0 +1,11 @@
+package org.java.mentorship.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) { ApplicationContext ctx = SpringApplication.run(Application.class, args); }
+}
