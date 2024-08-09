ef7b8d3e5f8d164364b42cf358d8d9dc403c87bf
andrei <35298550+d4rckh@users.noreply.github.com>
2024-08-09 13:58:37 +0300

    create /users/{id} in UserService

--- gateway-service/src/main/java/org/java/mentorship/gateway/controller/UserController.java
+++ gateway-service/src/main/java/org/java/mentorship/gateway/controller/UserController.java
@@ -4,20 +4,27 @@ import lombok.RequiredArgsConstructor;
 import org.java.mentorship.contracts.user.client.UserFeignClient;
 import org.java.mentorship.contracts.user.dto.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class HelloController {
+@RequestMapping("/users")
+public class UserController {
 
     private final UserFeignClient userFeignClient;
 
-    @GetMapping("/hello")
-    ResponseEntity<List<User>> hello() {
+    @GetMapping()
+    ResponseEntity<List<User>> getUsers() {
+        // TODO: Restrict to admin users
         return ResponseEntity.ok(userFeignClient.getUsers());
     }
+
+    @GetMapping("/{id}")
+    ResponseEntity<User> getUserById(@PathVariable(name="id") Integer id) {
+        // TODO: Users can't read other users
+        return ResponseEntity.ok(userFeignClient.getUser(id));
+    }
+
 }
--- user-service/src/main/java/org/java/mentorship/user/controller/UserController.java
+++ user-service/src/main/java/org/java/mentorship/user/controller/UserController.java
@@ -3,6 +3,7 @@ package org.java.mentorship.user.controller;
 import org.java.mentorship.contracts.user.dto.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -17,5 +18,14 @@ public class UserController {
         user.setEmail("test");
 
         return ResponseEntity.ok(Collections.singletonList(user));
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(name="id") Integer id) {
+        User user = new User();
+        user.setEmail("test");
+        user.setId(id);
+
+        return ResponseEntity.ok(user);
     }
 }
