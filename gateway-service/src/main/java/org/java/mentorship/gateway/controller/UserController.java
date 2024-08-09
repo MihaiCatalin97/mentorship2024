3ecefc7a4e2d20c6b82f6553c8433270219b0d7b
andrei <35298550+d4rckh@users.noreply.github.com>
2024-08-09 15:32:04 +0300

    SessionService and Repository + Controller.

--- gateway-service/src/main/java/org/java/mentorship/gateway/controller/UserController.java
+++ gateway-service/src/main/java/org/java/mentorship/gateway/controller/UserController.java
@@ -3,6 +3,7 @@ package org.java.mentorship.gateway.controller;
 import lombok.RequiredArgsConstructor;
 import org.java.mentorship.contracts.user.client.UserFeignClient;
 import org.java.mentorship.contracts.user.dto.User;
+import org.java.mentorship.contracts.user.dto.UserRegistrationRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +26,12 @@ public class UserController {
     ResponseEntity<User> getUserById(@PathVariable(name="id") Integer id) {
         // TODO: Users can't read other users
         return ResponseEntity.ok(userFeignClient.getUser(id));
+    }
+
+    @PostMapping("/register")
+    ResponseEntity<User> registerUser(@RequestBody UserRegistrationRequest registrationRequest) {
+        // TODO: Users can't read other users
+        return ResponseEntity.ok(userFeignClient.registerUser(registrationRequest));
     }
 
 }
--- user-service/src/main/java/org/java/mentorship/user/service/UserService.java
+++ user-service/src/main/java/org/java/mentorship/user/service/UserService.java
@@ -1,15 +1,40 @@
 package org.java.mentorship.user.service;
 
+import lombok.AllArgsConstructor;
+import org.java.mentorship.contracts.user.dto.User;
+import org.java.mentorship.contracts.user.dto.UserRegistrationRequest;
 import org.java.mentorship.user.domain.UserEntity;
 import org.java.mentorship.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class UserService {
     UserRepository repository;
+
+    public String getMd5(String plaintext)
+    {
+        MessageDigest m = null;
+        try {
+            m = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        m.reset();
+        m.update(plaintext.getBytes());
+        byte[] digest = m.digest();
+        BigInteger bigInt = new BigInteger(1,digest);
+        String hashtext = bigInt.toString(16);
+
+        hashtext = "0".repeat(32 - hashtext.length()) + hashtext;
+        return hashtext;
+    }
 
     public List<UserEntity> getAllUsers() {
         return repository.getAllUsers();
@@ -17,5 +42,31 @@ public class UserService {
 
     public Optional<UserEntity> getUserById(Integer id) {
         return repository.getUserById(id);
+    }
+
+    public Optional<UserEntity> getUserByEmail(String email) {
+        return repository.getUserByEmail(email);
+    }
+
+    public boolean registerUser(UserRegistrationRequest registrationRequest) {
+        if (getUserByEmail(registrationRequest.getEmail()).isPresent()) {
+            return false;
+        }
+
+        final UserEntity user = new UserEntity();
+        user.setEmail(registrationRequest.getEmail());
+        user.setFirstName(registrationRequest.getFirstName());
+        user.setLastName(registrationRequest.getLastName());
+        user.setVerified(false);
+        user.setHashedPassword(getMd5(registrationRequest.getPassword()));
+        System.out.println(user);
+        return repository.saveUser(user);
+    }
+
+    public boolean verifyUserHash(Integer id, String password) {
+        Optional<UserEntity> user = getUserById(id);
+        return user
+                .map(userEntity -> userEntity.getHashedPassword().equals(getMd5(password)))
+                .orElse(false);
     }
 }
