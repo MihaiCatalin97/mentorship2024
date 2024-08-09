e7d8ea5b0671e900d4d715796d3a18afb71a23f3
Sorana09 <sorananeculaed@gmail.com>
2024-08-09 16:50:26 +0300

    reformat code

--- notification-service/src/main/java/org/java/mentorship/notification/repository/NotificationRepository.java
+++ notification-service/src/main/java/org/java/mentorship/notification/repository/NotificationRepository.java
@@ -1,22 +1,38 @@
 package org.java.mentorship.notification.repository;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.java.mentorship.contracts.notification.dto.Notification;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
-@Data
 @AllArgsConstructor
 public class NotificationRepository {
     List<Notification> notifications = new ArrayList<>();
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public Notification getNotificationById(Integer id) {
+        return notifications.stream().filter(notification -> notification.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    public Notification updateNotification(Integer id, Notification notification) {
+        // search and update
+        notification.setMarkedAsRead(true);
+        return notification;
+    }
 
     public Notification createNotification(Notification notification) {
         notifications.add(notification);
         return notification;
     }
 
+    public List<Notification> getWebNotificationsByUser(Integer userId) {
+        return notifications.stream().filter(notification -> notification.getUserId().equals(userId)).collect(Collectors.toList());
+    }
 }
