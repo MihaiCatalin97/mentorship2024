9eadbb8a45b5d4846068716b5ea8340f5524ccb8
d4rckh <35298550+d4rckh@users.noreply.github.com>
2024-08-13 14:36:12 +0300

    move deps to contracts pom.xml

--- user-service/src/main/java/org/java/mentorship/user/repository/mapper/SessionMapper.java
+++ user-service/src/main/java/org/java/mentorship/user/repository/mapper/SessionMapper.java
@@ -4,12 +4,37 @@ import org.apache.ibatis.annotations.*;
 import org.java.mentorship.contracts.user.dto.Session;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Mapper
 public interface SessionMapper {
     @Select("SELECT * FROM sessions")
     List<Session> findAll();
+
+    @Select("""
+        <script>
+            SELECT * FROM sessions
+            <where>
+                <if test="id!=null">
+                    AND id=#{id}
+                </if>
+                <if test="sesssionKey!=null">
+                    AND session_key=#{sessionKey}
+                </if>
+                <if test="expired==false">
+                    AND expires_at > NOW()
+                </if>
+                <if test="expired==true">
+                    AND expires_at <= NOW()
+                </if>
+                <if test="userId!=null">
+                    AND user_id=#{userId}
+                </if>
+            </where>
+        </script>
+    """)
+    List<Session> find(Map<String, Object> queries);
 
     @Insert("INSERT INTO sessions(session_key, expires_at, user_id) VALUES (#{sessionKey}, #{expiresAt}, #{userId})")
     @Options(useGeneratedKeys = true, keyColumn = "id")
