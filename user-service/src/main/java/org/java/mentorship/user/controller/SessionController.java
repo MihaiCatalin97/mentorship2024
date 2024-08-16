77c96c775147f3646333e737a7bda944143a586b
d4rckh <35298550+d4rckh@users.noreply.github.com>
2024-08-16 15:13:10 +0300

    add session with key contract for security reasons

--- user-service/src/main/java/org/java/mentorship/user/controller/SessionController.java
+++ user-service/src/main/java/org/java/mentorship/user/controller/SessionController.java
@@ -2,12 +2,16 @@ package org.java.mentorship.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.java.mentorship.contracts.user.dto.Session;
+import org.java.mentorship.contracts.user.dto.SessionWithKey;
 import org.java.mentorship.contracts.user.dto.request.LoginRequest;
+import org.java.mentorship.user.domain.SessionEntity;
+import org.java.mentorship.user.domain.mapper.SessionContractMapper;
 import org.java.mentorship.user.service.SessionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,22 +19,23 @@ public class SessionController {
     private final SessionService sessionService;
 
     @PostMapping("/sessions")
-    public ResponseEntity<Session> createSession(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<SessionWithKey> createSession(@RequestBody LoginRequest loginRequest) {
         return sessionService.createSession(loginRequest)
-                .map(ResponseEntity::ok)
+                .map(session -> ResponseEntity.ok(SessionContractMapper.sessionToContractWithKey(session)))
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
     @GetMapping("/sessions")
     public ResponseEntity<List<Session>> getSessions(@RequestParam(name = "userId", required = false) Integer userId,
                                                      @RequestParam(name = "isActive", required = false) Boolean isActive) {
-        return ResponseEntity.ok(sessionService.find(userId, isActive));
+        return ResponseEntity.ok(sessionService.find(userId, isActive)
+                .stream().map(SessionContractMapper::sessionToContract).collect(Collectors.toList()));
     }
 
     @GetMapping("/sessions/{key}")
     public ResponseEntity<Session> getSession(@PathVariable(name = "key") String key) {
         return sessionService.getSession(key)
-                .map(ResponseEntity::ok)
+                .map(session -> ResponseEntity.ok(SessionContractMapper.sessionToContract(session)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- user-service/src/main/java/org/java/mentorship/user/repository/SessionRepository.java
+++ user-service/src/main/java/org/java/mentorship/user/repository/SessionRepository.java
@@ -1,10 +1,8 @@
 package org.java.mentorship.user.repository;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
-import org.java.mentorship.contracts.user.dto.Session;
+import org.java.mentorship.user.domain.SessionEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,10 +19,10 @@ public interface SessionRepository {
                 </where>
             </script> 
             """)
-    List<Session> find(Integer userId);
+    List<SessionEntity> find(Integer userId);
 
-    void insert(Session session);
+    void insert(SessionEntity session);
 
     @Select("SELECT * FROM sessions WHERE session_key=#{sessionKey}")
-    Optional<Session> getByKey(String key);
+    Optional<SessionEntity> getByKey(String key);
 }
