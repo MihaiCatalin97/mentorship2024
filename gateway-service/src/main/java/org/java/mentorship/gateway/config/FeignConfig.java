2f3ffc89048afe6b2417b82dbe2fc4b74eedab42
andrei <35298550+d4rckh@users.noreply.github.com>
2024-08-21 15:34:23 +0300

    fix feign handler

--- gateway-service/src/main/java/org/java/mentorship/gateway/config/FeignConfig.java
+++ gateway-service/src/main/java/org/java/mentorship/gateway/config/FeignConfig.java
@@ -2,7 +2,6 @@ package org.java.mentorship.gateway.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.codec.ErrorDecoder;
-import org.java.mentorship.gateway.exception.handler.FeignHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +9,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignConfig {
 
-    @Bean @Autowired
-    public ErrorDecoder errorDecoder(ObjectMapper objectMapper) {
-        return new FeignHandler(objectMapper);
-    }
 }
