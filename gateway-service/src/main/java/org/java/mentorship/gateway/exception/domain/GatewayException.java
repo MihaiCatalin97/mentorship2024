dbd1267a6cc0555c0cc755c19f4b0fdfafc25807
andrei <35298550+d4rckh@users.noreply.github.com>
2024-08-21 15:51:30 +0300

    pass source exception to gateway exception and print json processing errors

--- gateway-service/src/main/java/org/java/mentorship/gateway/exception/domain/GatewayException.java
+++ gateway-service/src/main/java/org/java/mentorship/gateway/exception/domain/GatewayException.java
@@ -5,17 +5,28 @@ import lombok.EqualsAndHashCode;
 import org.java.mentorship.contracts.common.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 
+import java.util.Optional;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class GatewayException extends RuntimeException {
 
     private final HttpStatus statusCode;
     private final ErrorResponse errorResponse;
+    private final Optional<RuntimeException> sourceException;
 
     public GatewayException(ErrorResponse errorResponse, HttpStatus statusCode) {
         super(errorResponse.getError());
         this.statusCode = statusCode;
         this.errorResponse = errorResponse;
+        this.sourceException = Optional.empty();
+    }
+
+    public GatewayException(ErrorResponse errorResponse, RuntimeException sourceException, HttpStatus statusCode) {
+        super(errorResponse.getError(), sourceException);
+        this.statusCode = statusCode;
+        this.errorResponse = errorResponse;
+        this.sourceException = Optional.of(sourceException);
     }
 
 }
