e7a942ff52ee8ebb5ad8be960e824cb4a05e975f
andrei <35298550+d4rckh@users.noreply.github.com>
2024-08-22 10:53:35 +0300

    fix tests

--- gateway-service/src/main/java/org/java/mentorship/gateway/exception/domain/GatewayException.java
+++ gateway-service/src/main/java/org/java/mentorship/gateway/exception/domain/GatewayException.java
@@ -13,20 +13,17 @@ public class GatewayException extends RuntimeException {
 
     private final HttpStatus statusCode;
     private final ErrorResponse errorResponse;
-    private final Optional<RuntimeException> sourceException;
 
     public GatewayException(ErrorResponse errorResponse, HttpStatus statusCode) {
         super(errorResponse.getError());
         this.statusCode = statusCode;
         this.errorResponse = errorResponse;
-        this.sourceException = Optional.empty();
     }
 
     public GatewayException(ErrorResponse errorResponse, RuntimeException sourceException, HttpStatus statusCode) {
         super(errorResponse.getError(), sourceException);
         this.statusCode = statusCode;
         this.errorResponse = errorResponse;
-        this.sourceException = Optional.of(sourceException);
     }
 
 }
