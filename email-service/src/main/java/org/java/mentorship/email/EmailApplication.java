f9443fd7b7fae4efd3dc1cc6127bdf6df0ebb81e
andrei <35298550+d4rckh@users.noreply.github.com>
2024-08-22 13:52:06 +0300

    separate secrets

--- email-service/src/main/java/org/java/mentorship/email/EmailApplication.java
+++ email-service/src/main/java/org/java/mentorship/email/EmailApplication.java
@@ -6,6 +6,7 @@ import org.java.mentorship.email.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
+@PropertySource("classpath:email.properties")
 public class EmailApplication {
 
     @Autowired
