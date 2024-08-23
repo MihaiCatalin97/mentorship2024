d5b89f45417a0578288185b903b0bfdb6ea2bbd1
SpatariuIoanGabriel <sp.ionut02@gmail.com>
2024-08-23 13:41:53 +0300

    Resolved coverage

--- calculation-service/src/main/java/org/java/mentorship/calculation/config/SpringConfig.java
+++ calculation-service/src/main/java/org/java/mentorship/calculation/config/SpringConfig.java
@@ -3,7 +3,9 @@ package org.java.mentorship.calculation.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import lombok.Generated;
 
+@Generated
 @Configuration
 @EnableScheduling
 @PropertySource("classpath:task.properties")
