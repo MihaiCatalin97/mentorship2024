af788611ff426cbe709f4057081c2fbfdb846636
Andrei U <ureche96@gmail.com>
2024-08-26 14:34:23 +0300

    added stuff to contracts dto

--- contracts/email-contracts/src/main/java/org/java/mentorship/contracts/email/dto/EmailRequest.java
+++ contracts/email-contracts/src/main/java/org/java/mentorship/contracts/email/dto/EmailRequest.java
@@ -1,11 +1,15 @@
 package org.java.mentorship.contracts.email.dto;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotNull;
 
 @Data
 public class EmailRequest {
 
+    @NotNull(message = "to cannot be null")
     private String to;
+
+    @NotNull(message = "subject cannot be null")
     private String subject;
 
 }
